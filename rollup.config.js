e683fbb2653d3153c20722d488b73b47d8f2c815
Matt Travi <matt@gaincompliance.com>
2018-10-15 11:35:26 -0500

    build(rollup): leveraged the auto-external plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
+import autoExternal from 'rollup-plugin-auto-external';
 
 export default {
+  plugins: [autoExternal()],
   input: 'src/index.js',
-  external: [
-    '@travi/project-scaffolder',
-    '@travi/javascript-scaffolder'
-  ],
   output: [
     {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
     {file: 'lib/index.es.js', format: 'es', sourcemap: true}
