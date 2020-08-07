687467bf4ba86d384a86122aa3ddeb4cd591887d
Trevor Richardson <trevor@gaincompliance.com>
2020-08-07 14:55:51 -0500

    feat(enhanced-scaffolders): add jest & mocha test scaffold

--- src/enhanced-scaffolders.js
+++ src/enhanced-scaffolders.js
@@ -3,6 +3,8 @@ import {prompt} from '@travi/github-scaffolder';
 import {scaffold as scaffoldTravis} from '@travi/travis-scaffolder-javascript';
 import {scaffold as scaffoldAppEngine} from '@travi/node-app-engine-standard-scaffolder';
 import {scaffold as scaffoldHapi} from '@form8ion/hapi-scaffolder';
+import {scaffold as scaffoldJest} from '@form8ion/jest-scaffolder';
+import {scaffold as scaffoldMocha} from '@form8ion/mocha-scaffolder';
 import {scaffold as scaffoldReactComponents} from '@form8ion/react-components-scaffolder';
 
 export function javascript(options) {
@@ -18,6 +20,7 @@ export function javascript(options) {
       npmAccount: 'gaincompliance',
       author: {name: 'Gain Compliance', url: 'https://github.com/GainCompliance'}
     },
+    unitTestFrameworks: {mocha: {scaffolder: scaffoldMocha}, jest: {scaffolder: scaffoldJest}},
     ciServices: {Travis: {scaffolder: scaffoldTravis, public: true, private: true}},
     hosts: {
       'App Engine Standard': {projectTypes: ['node'], scaffolder: scaffoldAppEngine}
--- test/unit/enhanced-scaffolders-test.js
+++ test/unit/enhanced-scaffolders-test.js
@@ -3,6 +3,8 @@ import * as githubScaffolder from '@travi/github-scaffolder';
 import {scaffold as scaffoldTravis} from '@travi/travis-scaffolder-javascript';
 import {scaffold as scaffoldAppEngine} from '@travi/node-app-engine-standard-scaffolder';
 import {scaffold as scaffoldHapi} from '@form8ion/hapi-scaffolder';
+import {scaffold as scaffoldJest} from '@form8ion/jest-scaffolder';
+import {scaffold as scaffoldMocha} from '@form8ion/mocha-scaffolder';
 import {scaffold as scaffoldReactComponents} from '@form8ion/react-components-scaffolder';
 import any from '@travi/any';
 import {assert} from 'chai';
@@ -37,6 +39,7 @@ suite('scaffolder factories', () => {
           npmAccount: 'gaincompliance',
           author: {name: 'Gain Compliance', url: 'https://github.com/GainCompliance'}
         },
+        unitTestFrameworks: {mocha: {scaffolder: scaffoldMocha}, jest: {scaffolder: scaffoldJest}},
         ciServices: {Travis: {scaffolder: scaffoldTravis, public: true, private: true}},
         hosts: {
           'App Engine Standard': {projectTypes: ['node'], scaffolder: scaffoldAppEngine}
